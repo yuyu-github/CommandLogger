bc311352869ce287ad67e26b33ff84ab06bf0286
yuyu <hcsd7b34@gmail.com>
2022-12-16 23:27:59 +0900

    Add: コマンドをファイルに保存

--- src/main/java/xyz/yuyu/commandlogger/CommandLogger.java
+++ src/main/java/xyz/yuyu/commandlogger/CommandLogger.java
@@ -1,13 +1,37 @@
 package xyz.yuyu.commandlogger;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.command.BlockCommandSender;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.UUID;
+
 public final class CommandLogger extends JavaPlugin {
+  static Plugin plugin;
+
+  Config config;
 
   @Override
   public void onEnable() {
+    plugin = this;
+
+    Bukkit.getServer().getPluginManager().registerEvents(new LogCommand(), this);
+
+    config = new Config(this);
+
     getLogger().info("プラグインが起動しました");
   }
 
